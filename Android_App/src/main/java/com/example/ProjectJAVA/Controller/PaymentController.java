17d5b512d86c76990a88c1bde9c4a2d79ca7416c
VoTaDuy <2251120073@ut.edu.vn>
2025-06-20 12:51:19 +0700

    git remote add origin https://github.com/VoTaDuy/AndroidApp.git

--- Android_App/src/main/java/com/example/ProjectJAVA/Controller/PaymentController.java
+++ Android_App/src/main/java/com/example/ProjectJAVA/Controller/PaymentController.java
@@ -1,11 +1,14 @@
 package com.example.ProjectJAVA.Controller;
 
+import com.example.ProjectJAVA.Entity.BookingSeats;
 import com.example.ProjectJAVA.Entity.Bookings;
 import com.example.ProjectJAVA.Entity.Payment;
 import com.example.ProjectJAVA.Enums.BookingStatus;
+import com.example.ProjectJAVA.Enums.SeatStatus;
 import com.example.ProjectJAVA.Payloads.Resquest.PaymentRequest;
 import com.example.ProjectJAVA.Repository.BookingRepository;
 import com.example.ProjectJAVA.Repository.PaymentRepository;
+import com.example.ProjectJAVA.Repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -26,8 +30,11 @@ public class PaymentController {
     @Autowired
     private BookingRepository bookingRepository;
 
+
+
     @PostMapping("/fake")
     public ResponseEntity<String> fakePayment(@RequestBody PaymentRequest request) {
+
         Bookings booking = bookingRepository.findById(request.getBookingId())
                 .orElseThrow(() -> new RuntimeException("Booking not exist with ID: " + request.getBookingId()));
 
@@ -39,7 +46,15 @@ public class PaymentController {
         paymentRepository.save(payment);
 
         booking.setBookingStatus(BookingStatus.BOOKED);
+        List<BookingSeats> seats = booking.getBookingSeatsList();
+        if (seats != null) {
+            for (BookingSeats bs : seats) {
+                bs.setStatus(SeatStatus.BOOKED);
+            }
+        }
         bookingRepository.save(booking);
+
+
 
         return ResponseEntity.ok("Success");
     }
